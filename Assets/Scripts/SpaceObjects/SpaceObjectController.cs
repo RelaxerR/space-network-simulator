b518925431421befc6ddd7ab7bff4b134d015dc7
Vyacheslav <relaxerrkis@gmail.com>
2025-05-08 12:05:07 +0400

    затычки

--- Assets/Scripts/SpaceObjects/SpaceObjectController.cs
+++ Assets/Scripts/SpaceObjects/SpaceObjectController.cs
@@ -20,12 +20,13 @@ public class SpaceObjectController : MonoBehaviour, IDynamicObject
 
     public Vector3 GetPositionFromTime(float time)
     {
-        
+        throw new NotImplementedException();
     }
 
     public Vector3 GetRotationFromTime(float time)
     {
         //Напиши метод GetRotationFromTime. Он должен рассчитывать вращение объекта из __spaceObject.RotationPeriod и _settings.RotationOffset (это параметр, отвечающий за смещение оси орбиты). Мне 
+        throw new NotImplementedException();
     }
 
     public void UpdatePosition()
