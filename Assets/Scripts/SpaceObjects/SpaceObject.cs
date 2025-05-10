31a603ddef545318f506f50b992ec5e228e93f4d
Vyacheslav <relaxerrkis@gmail.com>
2025-05-10 13:37:35 +0400

    Add self-orbiting properties to SpaceObject: include Radius, OrbitalPeriod, and RotationAxisTilt

--- Assets/Scripts/SpaceObjects/SpaceObject.cs
+++ Assets/Scripts/SpaceObjects/SpaceObject.cs
@@ -6,6 +6,15 @@ public class SpaceObject
     public bool IsStatic { get; private set; }
     public float DistanceToStatic { get; private set; }
     public float Mass { get; private set; }
+
+    #region self-orbiting
+
+    public float Radius { get; private set; } // Радиус объекта в метрах
+    public float OrbitalPeriod { get; private set; } // Период одного оборота вокруг своей оси в секунду
+    public float RotationAxisTilt { get; private set; } // Угол наклона оси вращения в градусах
+
+    #endregion
+    
     public Vector3 Velocity { get; set; } // Add this property to store velocity
     
     public SpaceObject(string name, bool isStatic, float distanceToStatic, float mass)
