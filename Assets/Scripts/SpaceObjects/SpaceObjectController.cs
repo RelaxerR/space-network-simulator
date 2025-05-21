53aa70bd0d6aa164f90e08cbb6b695483ed5c575
Vyacheslav <relaxerrkis@gmail.com>
2025-05-21 19:43:54 +0400

    Add Aerial class and update orbital properties for satellites: modify AngularVelocity and add LineRenderer for orbit visualization

--- Assets/Scripts/SpaceObjects/SpaceObjectController.cs
+++ Assets/Scripts/SpaceObjects/SpaceObjectController.cs
@@ -1,10 +1,29 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(LineRenderer))]
 public class SpaceObjectController : MonoBehaviour, IDynamicObject
 {
     [SerializeField] private SpaceObjectController StaticSpaceObject;
     [SerializeField] private SpaceObjectSettings _settings;
+
+    
+
+    private LineRenderer _lineRenderer;
+    private List<Vector3> _orbitPoints = new List<Vector3>();
+    [SerializeField] private float minDistance = 0.1f; // Minimum distance to add a new point
+
+    private void Awake()
+    {
+        #region Отображение орбит
+        _lineRenderer = GetComponent<LineRenderer>();
+        _orbitPoints.Clear();
+        _orbitPoints.Add(transform.position);
+        _lineRenderer.positionCount = 1;
+        _lineRenderer.SetPosition(0, transform.position);
+        #endregion
+    }
     
     private static GlobalSettings _globalSettings => GlobalSettings.Instance;
 
@@ -64,5 +83,15 @@ public class SpaceObjectController : MonoBehaviour, IDynamicObject
     {
         transform.position = GetPositionFromTime(time);
         transform.rotation = Quaternion.Euler(GetRotationFromTime(time));
+        
+        #region Отображение орбит
+        // Add new point if moved enough
+        if (Vector3.Distance(_orbitPoints[_orbitPoints.Count - 1], transform.position) > minDistance)
+        {
+            _orbitPoints.Add(transform.position);
+            _lineRenderer.positionCount = _orbitPoints.Count;
+            _lineRenderer.SetPositions(_orbitPoints.ToArray());
+        }
+        #endregion
     }
 }
